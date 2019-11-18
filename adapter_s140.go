e75f949904d345acb9f265d806c40d63ea2fe09a
Nikolas Sepos <nikolas.sepos@gmail.com>
2019-11-18 20:19:32 +0200

    Port current codebase to S140 softdevice

---
+++ adapter_s140.go
@@ -0,0 +1,10 @@
+// +build softdevice,s140v7
+
+package bluetooth
+
+/*
+// Add the correct SoftDevice include path to CFLAGS, so #include will work as
+// expected.
+#cgo CFLAGS: -Is140_nrf52_7.0.1/s140_nrf52_7.0.1_API/include
+*/
+import "C"
