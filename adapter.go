f2f9a517d0909b844389d92fe33ff0c0421334e9
Ayke van Laethem <aykevanlaethem@gmail.com>
2020-05-25 17:26:24 +0200

    Add flag for debug printing
    This should normally be false, but can be set to true for debugging.

--- adapter.go
+++ adapter.go
@@ -1,5 +1,8 @@
 package bluetooth
 
+// Set this to true to print debug messages, for example for unknown events.
+const debug = false
+
 // SetEventHandler sets the callback that gets called on incoming events.
 //
 // Warning: must only be called when the Bluetooth stack has not yet been
