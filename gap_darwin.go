5f44bb4a96a5cf5e7ed6fd89225475dfc5102ea7
Ron Evans <ron@hybridgroup.com>
2020-08-28 13:31:17 +0200

    macos: completed initial implementation
    Signed-off-by: Ron Evans <ron@hybridgroup.com>

--- gap_darwin.go
+++ gap_darwin.go
@@ -2,27 +2,26 @@ package bluetooth
 
 import (
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/JuulLabs-OSS/cbgo"
 )
 
-// Address contains a Bluetooth address, which is a MAC address plus some extra
-// information.
+// Address contains a Bluetooth address, which on macOS instead of a MAC address
+// is instead a UUID.
 type Address struct {
-	// UUID if this is macOS.
+	// UUID since this is macOS.
 	UUID
-
-	isRandom bool
 }
 
-// IsRandom if the address is randomly created.
+// IsRandom ignored on macOS.
 func (ad Address) IsRandom() bool {
-	return ad.isRandom
+	return false
 }
 
-// SetRandom if is a random address.
+// SetRandom ignored on macOS.
 func (ad Address) SetRandom(val bool) {
-	ad.isRandom = val
 }
 
 // Set the address
@@ -37,7 +36,7 @@ func (a *Adapter) Scan(callback func(*Adapter, ScanResult)) (err error) {
 		return errors.New("must provide callback to Scan function")
 	}
 
-	if a.cancelChan != nil {
+	if a.scanChan != nil {
 		return errors.New("already calling Scan function")
 	}
 
@@ -46,24 +45,21 @@ func (a *Adapter) Scan(callback func(*Adapter, ScanResult)) (err error) {
 	// Channel that will be closed when the scan is stopped.
 	// Detecting whether the scan is stopped can be done by doing a non-blocking
 	// read from it. If it succeeds, the scan is stopped.
-	cancelChan := make(chan struct{})
-	a.cancelChan = cancelChan
+	a.scanChan = make(chan error)
 
 	a.cm.Scan(nil, &cbgo.CentralManagerScanOpts{
-		AllowDuplicates: true,
+		AllowDuplicates: false,
 	})
 
-	for {
-		// Check whether the scan is stopped. This is necessary to avoid a race
-		// condition between the signal channel and the cancelScan channel when
-		// the callback calls StopScan() (no new callbacks may be called after
-		// StopScan is called).
-		select {
-		case <-cancelChan:
-			// stop scanning here?
-			return nil
-		default:
-		}
+	// Check whether the scan is stopped. This is necessary to avoid a race
+	// condition between the signal channel and the cancelScan channel when
+	// the callback calls StopScan() (no new callbacks may be called after
+	// StopScan is called).
+	select {
+	case <-a.scanChan:
+		close(a.scanChan)
+		a.scanChan = nil
+		return nil
 	}
 }
 
@@ -71,13 +67,67 @@ func (a *Adapter) Scan(callback func(*Adapter, ScanResult)) (err error) {
 // callback to stop the current scan. If no scan is in progress, an error will
 // be returned.
 func (a *Adapter) StopScan() error {
-	if a.cancelChan != nil {
-		return errors.New("already calling Scan function")
+	if a.scanChan == nil {
+		return errors.New("not calling Scan function")
 	}
 
+	a.scanChan <- nil
 	a.cm.StopScan()
-	close(a.cancelChan)
-	a.cancelChan = nil
 
 	return nil
+}
+
+// Device is a connection to a remote peripheral.
+type Device struct {
+	cbgo.PeripheralDelegateBase
+
+	cm cbgo.CentralManager
+}
+
+// Connect starts a connection attempt to the given peripheral device address.
+func (a *Adapter) Connect(address Addresser, params ConnectionParams) (*Device, error) {
+	adr := address.(Address)
+	uuid, err := cbgo.ParseUUID(adr.UUID.String())
+	if err != nil {
+		return nil, err
+	}
+	prphs := a.cm.RetrievePeripheralsWithIdentifiers([]cbgo.UUID{uuid})
+	if len(prphs) == 0 {
+		return nil, fmt.Errorf("Connect failed: no peer with address: %s", adr.UUID.String())
+	}
+	a.cm.Connect(prphs[0], nil)
+
+	// wait on channel for connect
+	select {
+	case p := <-a.connectChan:
+		d := &Device{
+			cm: a.cm,
+		}
+
+		p.SetDelegate(d)
+		return d, nil
+	case <-time.NewTimer(10 * time.Second).C:
+		return nil, errors.New("timeout on Connect")
+	}
+}
+
+// Peripheral delegate functions
+
+func (d *Device) DidDiscoverServices(prph cbgo.Peripheral, err error) {
+}
+func (d *Device) DidDiscoverCharacteristics(prph cbgo.Peripheral, svc cbgo.Service, err error) {
+}
+func (d *Device) DidDiscoverDescriptors(prph cbgo.Peripheral, chr cbgo.Characteristic, err error) {
+}
+func (d *Device) DidUpdateValueForCharacteristic(prph cbgo.Peripheral, chr cbgo.Characteristic, err error) {
+}
+func (d *Device) DidUpdateValueForDescriptor(prph cbgo.Peripheral, dsc cbgo.Descriptor, err error) {
+}
+func (d *Device) DidWriteValueForCharacteristic(prph cbgo.Peripheral, chr cbgo.Characteristic, err error) {
+}
+func (d *Device) DidWriteValueForDescriptor(prph cbgo.Peripheral, dsc cbgo.Descriptor, err error) {
+}
+func (d *Device) DidUpdateNotificationState(prph cbgo.Peripheral, chr cbgo.Characteristic, err error) {
+}
+func (d *Device) DidReadRSSI(prph cbgo.Peripheral, rssi int, err error) {
 }
