9647735fcaf6157e74b86113a5a73af39f03e581
deadprogram <ron@hybridgroup.com>
2020-09-07 22:39:05 +0200

    docs,domain: use import for custom domain for package, also GoDocs format info for package
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ bluetooth.go
@@ -0,0 +1,9 @@
+// Package bluetooth provides a cross-platform Bluetooth module for Go
+// that can be used on operating systems such as Linux, macOS, and Windows.
+//
+// It can also be used running "bare metal" on microcontrollers such as
+// those produced by Nordic Semiconductor.
+//
+// This package can be use to create Bluetooth Low Energy centrals as well as peripherals.
+//
+package bluetooth // import "tinygo.org/x/bluetooth"
