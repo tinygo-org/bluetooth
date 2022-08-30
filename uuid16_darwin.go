0b701c55ca1c75abf27e54e7e78b8101de826320
deadprogram <ron@hybridgroup.com>
2022-08-30 19:59:42 +0200

    macos: use the same UUID format as expected by other standard
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- uuid16_darwin.go
+++ uuid16_darwin.go
@@ -7,9 +7,9 @@ package bluetooth
 func New16BitUUID(shortUUID uint16) UUID {
 	// mac OS uses a unique format for UUID.
 	var uuid UUID
-	uuid[0] = 0x00000000
-	uuid[1] = 0x00000000
-	uuid[2] = 0x00000000
+	uuid[0] = 0x5F9B34FB
+	uuid[1] = 0x80000080
+	uuid[2] = 0x00001000
 	uuid[3] = uint32(shortUUID) << 16
 	return uuid
 }
