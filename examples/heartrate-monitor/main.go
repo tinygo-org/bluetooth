854f44648c101fbbb2c870f274ef3dca1e3d8f7b
Erik de Vries <e.j.de.vries@gmail.com>
2023-08-08 22:07:25 +0200

    Update dependencies - add documentation to heartrate-monitor

--- examples/heartrate-monitor/main.go
+++ examples/heartrate-monitor/main.go
@@ -3,20 +3,24 @@
 //
 // Once connected, it subscribes to notifications for the data value, and
 // displays it.
+// The Heart Rate Measurement characteristic is a variable-length structure (array) containing a Flags field, a Heart
+// Rate Measurement Value field and, based on the contents of the Flags field, may contain additional fields
+// such as Energy Expended or RR-Interval.
+// More info can be found here: https://www.bluetooth.com/specifications/specs/gatt-specification-supplement-6/
+// In this example only the heart rate is used, this is the second element in the array of bytes.
 //
 // To run this on a desktop system:
 //
-// 		go run ./examples/heartrate-monitor EE:74:7D:C9:2A:68
+//	go run ./examples/heartrate-monitor EE:74:7D:C9:2A:68
 //
 // To run this on a microcontroller, change the constant value in the file
 // "mcu.go" to set the MAC address of the device you want to discover.
 // Then, flash to the microcontroller board like this:
 //
-//		tinygo flash -o circuitplay-bluefruit ./examples/heartrate-monitor
+//	tinygo flash -o circuitplay-bluefruit ./examples/heartrate-monitor
 //
 // Once the program is flashed to the board, connect to the USB port
 // via serial to view the output.
-//
 package main
 
 import (
--- go.mod
+++ go.mod
@@ -4,20 +4,20 @@ go 1.18
 
 require (
 	github.com/go-ole/go-ole v1.2.6
-	github.com/godbus/dbus/v5 v5.0.3
-	github.com/muka/go-bluetooth v0.0.0-20220830075246-0746e3a1ea53
-	github.com/saltosystems/winrt-go v0.0.0-20230613063811-c792451fa808
+	github.com/godbus/dbus/v5 v5.1.0
+	github.com/muka/go-bluetooth v0.0.0-20221213043340-85dc80edc4e1
+	github.com/saltosystems/winrt-go v0.0.0-20230710111611-a39229b5054c
 	github.com/tinygo-org/cbgo v0.0.4
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/crypto v0.12.0
 	tinygo.org/x/drivers v0.25.0
-	tinygo.org/x/tinyterm v0.1.0
+	tinygo.org/x/tinyfont v0.4.0
+	tinygo.org/x/tinyterm v0.3.0
 )
 
 require (
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	tinygo.org/x/tinyfont v0.4.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
 )
