132d7deb49772f1fc0f062293f5ad3bf400a00b9
daniel <dnlwgnd@gmail.com>
2024-02-17 22:55:25 +0100

    added a test case for ServiceData

--- gap_test.go
+++ gap_test.go
@@ -55,6 +55,17 @@ func TestCreateAdvertisementPayload(t *testing.T) {
 				},
 			},
 		},
+		{
+			raw: "\x02\x01\x06" + // flags
+				"\x0B\x09\x44\x49\x59\x2D\x73\x65\x6E\x73\x6F\x72" + // local name
+				"\x0A\x16\xD2\xFC\x40\x02\xC4\x09\x03\xBF\x13", // service UUID
+			parsed: AdvertisementOptions{
+				LocalName: "DIY-sensor",
+				ServiceData: map[uint16]interface{}{
+					0xFCD2: []byte{0x40, 0x02, 0xC4, 0x09, 0x03, 0xBF, 0x13},
+				},
+			},
+		},
 	}
 	for _, tc := range tests {
 		var expectedRaw rawAdvertisementPayload
