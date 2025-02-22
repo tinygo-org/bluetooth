7030fac07bfdd62eb067002fd1b9970a8798f419
Chet Henry <chet.0x.henry@gmail.com>
2025-02-22 08:30:50 -0700

    Add tests that cover MarshalText, AppendText, UnmarshalBinary and AppendBinary

--- mac_test.go
+++ mac_test.go
@@ -6,9 +6,6 @@ import (
 )
 
 func TestParseMAC(t *testing.T) {
-	type args struct {
-		s string
-	}
 	tests := []struct {
 		name    string
 		strMac  string
@@ -127,5 +124,144 @@ func BenchmarkMacToString(b *testing.B) {
 	}
 	for i := 0; i < b.N; i++ {
 		_ = mac.String()
+	}
+}
+
+func TestMAC_UnmarshalBinary(t *testing.T) {
+	tests := []struct {
+		name    string
+		mac     []byte
+		wantMac MAC
+		wantErr bool
+	}{
+		{
+			name:    "incrementing",
+			mac:     []byte{188, 154, 120, 86, 52, 18},
+			wantMac: [6]byte{188, 154, 120, 86, 52, 18},
+			wantErr: false,
+		},
+		{
+			name:    "decrementing",
+			mac:     []byte{33, 67, 101, 135, 169, 203},
+			wantMac: [6]byte{33, 67, 101, 135, 169, 203},
+			wantErr: false,
+		},
+		{
+			name:    "normal",
+			mac:     []byte{204, 187, 170, 51, 34, 17},
+			wantMac: [6]byte{204, 187, 170, 51, 34, 17},
+			wantErr: false,
+		},
+		{
+			name:    "empty",
+			mac:     []byte{},
+			wantMac: [6]byte{},
+			wantErr: true,
+		},
+		{
+			name:    "extra",
+			mac:     []byte{33, 67, 101, 135, 169, 203, 255},
+			wantMac: [6]byte{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotMac := MAC{}
+			gotMac.UnmarshalBinary(tt.mac)
+
+			if !tt.wantErr && !bytes.Equal(gotMac[:], tt.wantMac[:]) {
+				t.Errorf("ParseMAC() = %v, want %v", [6]byte(gotMac), [6]byte(tt.wantMac))
+			}
+		})
+	}
+}
+
+func TestMAC_MarshalBinary(t *testing.T) {
+	tests := []struct {
+		name     string
+		mac      MAC
+		wantData []byte
+		wantErr  bool
+	}{
+		{
+			name:     "incrementing",
+			mac:      [6]byte{188, 154, 120, 86, 52, 18},
+			wantData: []byte{188, 154, 120, 86, 52, 18},
+			wantErr:  false,
+		},
+		{
+			name:     "decrementing",
+			mac:      [6]byte{33, 67, 101, 135, 169, 203},
+			wantData: []byte{33, 67, 101, 135, 169, 203},
+			wantErr:  false,
+		},
+		{
+			name:     "normal",
+			mac:      [6]byte{204, 187, 170, 51, 34, 17},
+			wantData: []byte{204, 187, 170, 51, 34, 17},
+			wantErr:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotData, err := tt.mac.MarshalBinary()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MAC.MarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr && !bytes.Equal(gotData, tt.wantData) {
+				t.Errorf("MAC.MarshalBinary() = %v, want %v", gotData, tt.wantData)
+			}
+		})
+	}
+}
+
+func TestMAC_MarshalUnmarshalBinary(t *testing.T) {
+	tests := []struct {
+		name    string
+		mac     MAC
+		wantMac MAC
+		wantErr bool
+	}{
+		{
+			name:    "incrementing",
+			mac:     [6]byte{188, 154, 120, 86, 52, 18},
+			wantMac: [6]byte{188, 154, 120, 86, 52, 18},
+			wantErr: false,
+		},
+		{
+			name:    "decrementing",
+			mac:     [6]byte{33, 67, 101, 135, 169, 203},
+			wantMac: [6]byte{33, 67, 101, 135, 169, 203},
+			wantErr: false,
+		},
+		{
+			name:    "normal",
+			mac:     [6]byte{204, 187, 170, 51, 34, 17},
+			wantMac: [6]byte{204, 187, 170, 51, 34, 17},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := tt.mac.MarshalBinary()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MAC.MarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			gotMac := &MAC{}
+
+			err = gotMac.UnmarshalBinary(b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MAC.UnmarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !tt.wantErr && !bytes.Equal(gotMac[:], tt.wantMac[:]) {
+				t.Errorf("MAC.MarshalBinary() = %v, want %v", gotMac, tt.wantMac)
+			}
+		})
 	}
 }
