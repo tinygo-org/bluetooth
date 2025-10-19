8a0bf633dc165630c6d0719537d19b15e646fff3
Tim Kluge <timklge@gmail.com>
2025-10-19 16:15:40 +0200

    Use short hand error check

--- gattc_linux.go
+++ gattc_linux.go
@@ -222,10 +222,8 @@ func (s DeviceService) DiscoverCharacteristics(uuids []UUID) ([]DeviceCharacteri
 // writes can be in flight at any given time. This call is also known as a
 // "write command" (as opposed to a write request).
 func (c DeviceCharacteristic) WriteWithoutResponse(p []byte) (n int, err error) {
-	args := make(map[string]any)
-	err = c.characteristic.Call("org.bluez.GattCharacteristic1.WriteValue", 0, p, args).Err
-	args["type"] = "command"
-	if err != nil {
+	args := map[string]any{"type": "command"}
+	if err := c.characteristic.Call("org.bluez.GattCharacteristic1.WriteValue", 0, p, args).Err; err != nil {
 		return 0, err
 	}
 	return len(p), nil
@@ -234,10 +232,8 @@ func (c DeviceCharacteristic) WriteWithoutResponse(p []byte) (n int, err error) 
 // Write replaces the characteristic value with a new value. The
 // call will return after all data has been written.
 func (c DeviceCharacteristic) Write(p []byte) (n int, err error) {
-	args := make(map[string]any)
-	args["type"] = "request"
-	err = c.characteristic.Call("org.bluez.GattCharacteristic1.WriteValue", 0, p, args).Err
-	if err != nil {
+	args := map[string]any{"type": "request"}
+	if err := c.characteristic.Call("org.bluez.GattCharacteristic1.WriteValue", 0, p, args).Err; err != nil {
 		return 0, err
 	}
 	return len(p), nil
