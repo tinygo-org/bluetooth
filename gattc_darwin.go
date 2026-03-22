088ca9707197b1981f696ec8c002ed324e40e031
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2026-03-22 06:21:20 -0400

    fix(darwin): Device.DiscoverCharacteristics() filters discovered characteristics
    Currently, even if you pass a slice of characteristics to discover. CoreBluetooth still discover ALL characteristics.
    
    As per the CoreBluetooth documentation: " If the characteristicUUIDs parameter is nil, this method returns all of the service’s characteristics. This is much slower than providing an array of characteristic UUIDs to search for."

--- gattc_darwin.go
+++ gattc_darwin.go
@@ -115,7 +115,14 @@ func (s DeviceService) UUID() UUID {
 // Passing a nil slice of UUIDs will return a complete list of
 // characteristics.
 func (s DeviceService) DiscoverCharacteristics(uuids []UUID) ([]DeviceCharacteristic, error) {
-	cbuuids := []cbgo.UUID{}
+	cbuuids := make([]cbgo.UUID, len(uuids))
+	for i, u := range uuids {
+		cbuuid, err := cbgo.ParseUUID(u.String())
+		if err != nil {
+			return nil, err
+		}
+		cbuuids[i] = cbuuid
+	}
 
 	s.device.prph.DiscoverCharacteristics(cbuuids, s.service)
 
