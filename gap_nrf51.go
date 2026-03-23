0f63a6448818f46057514a4f9e9661ac041505b3
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2026-03-23 15:16:25 -0400

    add stub implementation for s113v7

--- gap_nrf51.go
+++ gap_nrf51.go
@@ -108,3 +108,21 @@ func (a *Adapter) SetRandomAddress(mac MAC) error {
 	}
 	return nil
 }
+
+// Disconnect from the BLE device.
+func (d Device) Disconnect() error {
+	return errNotYetImplmented
+}
+
+// DiscoverServices starts a service discovery procedure.
+func (d Device) DiscoverServices(uuids []UUID) ([]DeviceService, error) {
+	return nil, errNotYetImplmented
+}
+
+// RequestConnectionParams requests a different connection latency and timeout
+// of the given device connection.
+func (d Device) RequestConnectionParams(params ConnectionParams) error {
+	return errNotYetImplmented
+}
+
+type DeviceService struct{}
