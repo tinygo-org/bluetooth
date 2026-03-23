b2463774be9c51600bd2f7ca034a47cecf3a100c
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2026-03-23 14:31:47 -0400

    remove stub implementations
    I did not realize the implemention was on others files

--- gap_sd.go
+++ gap_sd.go
@@ -15,19 +15,3 @@ type Device struct {
 
 	connectionHandle C.uint16_t
 }
-
-// Disconnect from the BLE device.
-func (d Device) Disconnect() error {
-	return errNotYetImplmented
-}
-
-// DiscoverServices starts a service discovery procedure.
-func (d Device) DiscoverServices(uuids []UUID) ([]DeviceService, error) {
-	return nil, errNotYetImplmented
-}
-
-// RequestConnectionParams requests a different connection latency and timeout
-// of the given device connection.
-func (d Device) RequestConnectionParams(params ConnectionParams) error {
-	return errNotYetImplmented
-}
